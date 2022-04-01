aeab5ab1b69eebae8d7f2d0741f7cda4f2867264
syc31354 <shenyuchao@offcn.com>
2022-04-01 14:36:29 +0800

    update zsh, nvim

--- vim/nvchad/custom/init.lua
+++ vim/nvchad/custom/init.lua
@@ -2,6 +2,14 @@
 -- This is where your custom modules and plugins go.
 -- Please check NvChad docs if you're totally new to nvchad + dont know lua!!
 -- MAPPINGS
+local custom_modules = {"custom.autocmds"}
+
+for _, module in ipairs(custom_modules) do
+    local ok, err = pcall(require, module)
+    if not ok then
+        error("Error loading " .. module .. "\n\n" .. err)
+    end
+end
 local map = require("core.utils").map
 
 map("n", "<leader>cc", ":Telescope <CR>")
